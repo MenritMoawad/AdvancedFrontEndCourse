5f22a71cf9a671bf8ea650305e9343ae64c7fad4
sohaghareb <soha.ghareb@student.guc.edu.eg>
2017-03-15 15:55:44 +0200

    dynamic number of slides but if the slides are multiple of the division

--- js/soha.js
+++ js/soha.js
@@ -2,37 +2,43 @@ jQuery(document).ready(function($) {
 	
 	//loop over all of them
 	$.each($('.myslider'), function() {
-	if($(this).attr('data-arrows') == 'true'){
-		var myslider=$(this);
-		$(this).append("<div class='slider_nav'><button class='prev'>Previous</button><button class='next'>Next</button></div>");
+		var slides_asked= parseInt($(this).attr('data-show-slides'));
 		var slide_count = $(this).find('.slide').length;
-		var current_slide = 0;
+		if(slides_asked >= slide_count)
+			slides_asked=slide_count-1;
+		var width=$(window).width();
+		var image_container_width=100*(slide_count/slides_asked);
+		var single_slider_width=100/slide_count -1;
+		$(this).find('.images_container').css('width', 100*(slide_count/slides_asked)+"%");
+		$(this).find('.slide').css('width', 100/slide_count -1+ "%");
 
-		$(this).find('.images_container').css('width', 100*slide_count +"%");
-		$(this).find('.slide').css('width', 100/slide_count + "%");
+		console.log(slides_asked);
+		if($(this).attr('data-arrows') == 'true'){
+			var myslider=$(this);
+			$(this).append("<div class='slider_nav'><button class='prev'>Previous</button><button class='next'>Next</button></div>");
+			var current_slide = 0;
+			$(this).find('.next').click(function(event) {
+				if (current_slide === slide_count-slides_asked) {
+					current_slide = 0;
+				}
 
-		$(this).find('.next').click(function(event) {
-			if (current_slide === slide_count-1) {
-				current_slide = 0;
-			}
+				else{
+					current_slide=current_slide+slides_asked;
+				}
+				// $('.images_container').animate({right: current_slide*100 + "%"}, 400);
+				myslider.find('.images_container').css('right', current_slide*single_slider_width*slides_asked + "%");
+			});
 
-			else{
-				current_slide++;
-			}
-			// $('.images_container').animate({right: current_slide*100 + "%"}, 400);
-			myslider.find('.images_container').css('right', current_slide*100 + "%");
-		});
-
-		$('.prev').click(function(event) {
-			if (current_slide === 0) {
-				current_slide = slide_count-1;
-			}
-			else{
-				current_slide--;
-			}
-			myslider.find('.images_container').css('right', current_slide*100 + "%");
-		});
-}
+			$('.prev').click(function(event) {
+				if (current_slide === 0) {
+					current_slide = slide_count-slides_asked;
+				}
+				else{
+					current_slide=current_slide-slides_asked;
+				}
+				myslider.find('.images_container').css('right', current_slide*single_slider_width*slides_asked + "%");
+			});
+	}
 
 });
 
