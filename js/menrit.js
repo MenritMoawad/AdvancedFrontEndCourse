37f889093d466ddf1734455f19daf4d6bddb238d
menritmoawad <menrit.moawad@gmail.com>
2017-03-17 13:49:12 +0200

    dropdown menu

--- js/menrit.js
+++ js/menrit.js
@@ -1,1 +1,20 @@
-
+$(document).foundation()
+$(document).ready(function() {
+  // var items = $('[data-modal]');
+  $('[data-modal]').hide();
+  $('[data-modal-btn]').click(function() {
+    console.log("ohayo");
+    var btnmodal = $(this).attr('data-modal-btn');
+    $('[data-modal=' + btnmodal + ']').toggle();
+    // var mod = items.attr('[data-modal]');
+    // console.log(mod);
+    var item = $('[data-modal=' + btnmodal + '] li');
+    console.log(item);
+    item.click(function() {
+      var txt = $(this).text();
+      console.log(txt);
+      $('[data-modal-btn=' + btnmodal + ']').text(txt);
+      $('[data-modal=' + btnmodal + ']').hide();
+    })
+  });
+})
