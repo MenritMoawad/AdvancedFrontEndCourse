9b69933f2e39b33786d39869f685259f23c0fdae
menritmoawad <menrit.moawad@gmail.com>
2017-03-28 00:13:20 +0200

    dropdown menu in blog page fixed

--- js/menrit.js
+++ js/menrit.js
@@ -1,13 +1,9 @@
 $(document).foundation()
 $(document).ready(function() {
-  // var items = $('[data-modal]');
   $('[data-modal]').hide();
   $('[data-modal-btn]').click(function() {
-    console.log("ohayo");
     var btnmodal = $(this).attr('data-modal-btn');
     $('[data-modal=' + btnmodal + ']').toggle();
-    // var mod = items.attr('[data-modal]');
-    // console.log(mod);
     var item = $('[data-modal=' + btnmodal + '] li');
     console.log(item);
     item.click(function() {
